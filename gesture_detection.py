c6dab8c2fe1db6999ed27d1edf58073655af6bfe
VaradMash <VaradMash@users.noreply.github.com>
2022-11-16 23:08:27 +0530

    Added template code for gesture detection.

---
+++ gesture_detection.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+class GestureDetection:
+
+    def __init__(self):
+        pass
+
+    def predict(self, landmarks):
+        """
+            Input   :   Landmarks
+            Utility :   Predict Signing status of user based on pose
+            Output  :   Boolean value (True -> Signing)
+        """
+        return True
