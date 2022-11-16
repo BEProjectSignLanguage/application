5a93dff5e8021d89528d08e68189cb2fcedcfcbc
VaradMash <VaradMash@users.noreply.github.com>
2022-11-16 23:09:01 +0530

    Added code for gesture interpretation.

---
+++ gesture_interpretation.py
@@ -0,0 +1,55 @@
+import numpy as np
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import LSTM, Dense
+
+class GestureInterpretation:
+
+    def __init__(self, output_units):
+        # Model architecture
+        self.model = Sequential(
+            [
+                LSTM(
+                    64, 
+                    return_sequences=True, 
+                    activation='relu', 
+                    input_shape=(30,1662)
+                ),
+                LSTM(
+                    128, 
+                    return_sequences=True, 
+                    activation='relu'
+                ),
+                LSTM(
+                    64, 
+                    return_sequences=False, 
+                    activation='relu'
+                ),
+                Dense(
+                    64, 
+                    activation='relu'
+                ),
+                Dense(
+                    32, 
+                    activation='relu'
+                ),
+                Dense(
+                    output_units,
+                    activation='softmax'
+                )
+            ]
+        )
+        # Model weights
+        self.model.load_weights(
+            './models/interpretation.h5'
+        )
+    
+    def predict(self, sequence):
+        """
+            Input   :   Sequence (frame buffer)
+            Utility :   Predict gesture on last frame
+            Output  :   Gesture (String)
+        """
+        result = self.model.predict(
+            np.expand_dims(sequence, axis=0)
+        )[0]
+        return result, np.argmax(result)
