895501eabb933c6cdf0b989031d7e8c65982cdd3
amaannaikwadi <amaannaikwadi@gmail.com>
2022-11-20 14:11:47 +0530

    QPushButton Modifications

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from subprocess import call
 #Import QApplication and all the required widgets
 import webcam_feed
 from PyQt6.QtWidgets import QApplication, QLabel, QWidget, QMainWindow,QPushButton
+from PyQt6.QtGui import QPixmap
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -10,15 +11,44 @@ class MainWindow(QMainWindow):
 
         self.setWindowTitle("My App")
         self.setWindowTitle("Sign Language detector App")
-        self.setContentsMargins(10,10,10,10)
+        self.setContentsMargins(100,100,100,100)
         self.started=False
 #                 left,top,width,height
         self.setGeometry(600, 250, 100, 100)
         # helloMsg = QLabel("<h1>Welcome, User!</h1>", parent=self)
         # helloMsg.move(0, 0)
 
+        self.label = QLabel(self)
+        pixmap = QPixmap("static\dhh.png")
+        self.label.setScaledContents(True)
+        self.label.move(100, 100)
+        self.label.setPixmap(pixmap)
+        self.label.setStyleSheet("""
+        *{
+            height: 100px;
+        }
+        """)
+        
         self.button = QPushButton("Connect to livefeed!")
-        self.button.setGeometry(150,150,50,50)
+        self.button.setStyleSheet("""
+        *{
+            margin-top: 125px;
+            margin-bottom: 0px;
+            height: 30%;
+            background-color: rgb(128, 60, 224);
+            border-style: outset;
+            border-width: 2px;
+            border-radius: 10px;
+            border-color: beige;
+            font: bold 14px;
+            padding: 6px;
+        }
+        QPushButton:hover{
+            background-color: rgb(193, 81, 241);
+        }
+        """)
+        self.button.move(200, 200)
+        self.button.setGeometry(100,100,50,50)
         self.button.setCheckable(True)
         self.button.clicked.connect(self.the_button_was_clicked)
         # button.move()
