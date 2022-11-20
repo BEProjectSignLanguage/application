47f150bbe676744e393030b2368c354b1bb2fa8e
amaannaikwadi <amaannaikwadi@gmail.com>
2022-11-21 01:35:42 +0530

    Image in app

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import sys
 from subprocess import call
 #Import QApplication and all the required widgets
 import webcam_feed
-from PyQt6.QtWidgets import QApplication, QLabel, QWidget, QMainWindow,QPushButton
+from PyQt6.QtWidgets import QApplication, QLabel, QWidget, QMainWindow,QPushButton, QGridLayout
 from PyQt6.QtGui import QPixmap
 
 class MainWindow(QMainWindow):
@@ -11,30 +11,24 @@ class MainWindow(QMainWindow):
 
         self.setWindowTitle("My App")
         self.setWindowTitle("Sign Language detector App")
-        self.setContentsMargins(100,100,100,100)
+        #self.setContentsMargins(100,100,100,100)
         self.started=False
 #                 left,top,width,height
-        self.setGeometry(600, 250, 100, 100)
+        self.setGeometry(600, 250, 290, 290)
         # helloMsg = QLabel("<h1>Welcome, User!</h1>", parent=self)
         # helloMsg.move(0, 0)
 
         self.label = QLabel(self)
         pixmap = QPixmap("static\dhh.png")
-        self.label.setScaledContents(True)
-        self.label.move(100, 100)
         self.label.setPixmap(pixmap)
-        self.label.setStyleSheet("""
-        *{
-            height: 100px;
-        }
-        """)
-        
+        self.label.setMinimumWidth(260)
+        self.label.setMinimumHeight(260)
+
         self.button = QPushButton("Connect to livefeed!")
         self.button.setStyleSheet("""
         *{
-            margin-top: 125px;
-            margin-bottom: 0px;
             height: 30%;
+            margin-top: 300%;
             background-color: rgb(128, 60, 224);
             border-style: outset;
             border-width: 2px;
@@ -47,12 +41,8 @@ class MainWindow(QMainWindow):
             background-color: rgb(193, 81, 241);
         }
         """)
-        self.button.move(200, 200)
-        self.button.setGeometry(100,100,50,50)
         self.button.setCheckable(True)
         self.button.clicked.connect(self.the_button_was_clicked)
-        # button.move()
-        # button.move(0,0)
         # Set the central widget of the Window.
         self.setCentralWidget(self.button)
 
