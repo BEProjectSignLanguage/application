ad202971d4ca12074906bc993623603e146a3262
VaradMash <VaradMash@users.noreply.github.com>
2023-03-19 22:15:37 +0530

    Integrated algorithm for keyword generation and segmentation.

--- webcam_feed.py
+++ webcam_feed.py
@@ -15,6 +15,19 @@ fmt = pyvirtualcam.PixelFormat.BGR
 # Defining configuration for camera
 handler = None
 
+def get_occurence_counter():
+    """
+        Input   :   None
+        Utility :   Get empty array for recording occurence
+        Output  :   list 
+    """
+    occurence_counter = [0 for i in range(len(config["actions"]))]
+    return occurence_counter
+
+# Initialize empty occurence counter for voting
+occurence_counter = get_occurence_counter()    
+keywords = []
+
 def run_feed():
     """
         Input   :   None
@@ -22,6 +35,8 @@ def run_feed():
         Output  :   None
     """
     global handler
+    global occurence_counter
+    global keywords
     # Defining constants
     frame_buffer = config["frame_buffer"]
     WIDTH = config["WIDTH"]
@@ -31,8 +46,11 @@ def run_feed():
         config["actions"]
     )
     interpretation_threshold = config["interpretation_threshold"]
-    previous = None
-    now = None
+    not_signing_interrupt_interval = config["not_signing_interrupt_interval"]
+
+    # Initialize variable to record start point and end point of not signing
+    start_not_signing = None
+    end_not_signing = None
 
     # Initialize Inferencing components
     landmarkDetection = PredictLandmarks()
@@ -42,13 +60,13 @@ def run_feed():
     )
     mp_holistic = landmarkDetection.get_holistic()
 
-    # Defining camera handler
-    # handler = cv2.VideoCapture(0)
+    # Defining camera handler    
     handler = cv2.VideoCapture(0)    
     handler.set(cv2.CAP_PROP_FRAME_WIDTH, WIDTH)
     handler.set(cv2.CAP_PROP_FRAME_HEIGHT, HEIGHT) 
     
-    sequence = []
+    sequence = []    
+    gesture = ""
 
     # Open mediapipe holistic
     with mp_holistic.Holistic(
@@ -63,9 +81,7 @@ def run_feed():
             fmt=fmt
         ) as cam: 
             # Run feed
-            while True:
-                now = datetime.now()
-                gesture = ""
+            while True:                                
                 ret, frame = handler.read()                
                 key = cv2.waitKey(1)
                 if ret:            
@@ -87,45 +103,57 @@ def run_feed():
                         -1 * frame_buffer:
                     ]
 
-                    # TODO : Replace "None" by landmarks
-                    is_signing = gestureDetection.predict(keypoints)                      
-                    if is_signing and len(sequence) == frame_buffer:
-                        # Run classification
+                    # TODO : Uncomment further 2 lines to start gesture detection
+                    # is_signing = gestureDetection.predict(keypoints)                      
+                    # if is_signing and len(sequence) == frame_buffer:
+                    # Run classification
+                    if len(sequence) == frame_buffer:
                         result, max_index = gestureInterpretation.predict(
                             sequence=sequence
                         )       
-                        # Apply threshold on classification
-                        if result[max_index] > interpretation_threshold:   
-                            gesture = actions[max_index]
+                        # TODO : Uncomment and indent to Apply threshold on classification                        
+                        # if result[max_index] > interpretation_threshold:                                                                          
+                        # gesture = actions[max_index]
+                        if max_index == 0:
+                            if start_not_signing is None:
+                                start_not_signing = datetime.now()
+                                end_not_signing = datetime.now()
+                            else:
+                                end_not_signing = datetime.now()
+                                # TODO  :   Implement thresholding logic
+                                delta = end_not_signing - start_not_signing
+                                if (delta.seconds + delta.microseconds * (10 ** -6)) >= not_signing_interrupt_interval:
+                                    # print("Not signing threshold exceeded ", delta.seconds)
+                                    maximum_occuring = np.argmax(occurence_counter)
+                                    # print("Occurence counter : ", maximum_occuring)
+                                    # Check most occurring
+                                    if np.argmax(occurence_counter) != 0:                                                                                
+                                        keywords.append(actions[maximum_occuring])
+                                        gesture += actions[maximum_occuring] + ", "
+                                        start_not_signing = None
+                                        end_not_signing = None                 
+                                        # Refresh occurence counter
+                                        occurence_counter = get_occurence_counter()                                        
+                                        print(gesture)               
+                        else:
+                            # Update frequency
+                            occurence_counter[max_index] += 1
                         frame = landmarkDetection.prob_viz(
                             res=result,
                             input_frame=frame,                    
                         )        
-                    cv2.putText(
-                        frame, 
-                        gesture, 
-                        (50, 50), 
-                        cv2.FONT_HERSHEY_COMPLEX, 
-                        2, 
-                        (0, 0, 0), 
-                        2, 
-                        cv2.LINE_AA
-                    )   
-                    # if previous is not None:
-                    #     delta = now - previous
-                    #     seconds = delta.seconds + delta.microseconds * (10 ** -6)
-                    #     cv2.putText(
-                    #         frame, 
-                    #         f"FPS : {1/seconds}", 
-                    #         (50, 300), 
-                    #         cv2.FONT_HERSHEY_COMPLEX, 
-                    #         2, 
-                    #         (0, 0, 0), 
-                    #         2, 
-                    #         cv2.LINE_AA
-                    #     )                      
+                    # cv2.putText(
+                    #     frame, 
+                    #     gesture, 
+                    #     (50, 50), 
+                    #     cv2.FONT_HERSHEY_COMPLEX, 
+                    #     2, 
+                    #     (0, 0, 0), 
+                    #     2, 
+                    #     cv2.LINE_AA
+                    # )                        
                     cam.send(frame)                
-                    cam.sleep_until_next_frame() 
+                    # cam.sleep_until_next_frame() 
                     # cv2.imshow("Feed", frame)
                 else:
                     print("Camera load failed")
