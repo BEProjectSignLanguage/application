f86f0c0f3625ad2909e7e9b9bbae216be80e87b5
VaradMash <VaradMash@users.noreply.github.com>
2023-03-20 11:28:06 +0530

    Added label placeholders and updation methods.

--- app.py
+++ app.py
@@ -15,23 +15,64 @@ class MainWindow(QWidget):
     def __init__(self):
         super().__init__()
         self.inference_allowed = False
-        self.webcamFeed = WebcamFeed()
+        self.webcamFeed = WebcamFeed(self)
 
         self.setWindowTitle("My App")
         self.setWindowTitle("Sign Language detector App")
         #self.setContentsMargins(100,100,100,100)
         self.started=False
 #                 left,top,width,height
-        self.setGeometry(600, 250, 290, 400)
+        #   Labels
+        self.setGeometry(600, 250, 290, 600)
         # helloMsg = QLabel("<h1>Welcome, User!</h1>", parent=self)
         # helloMsg.move(0, 0)
         layout = QVBoxLayout()
 
-        self.label = QLabel()
-        pixmap = QPixmap("static\pict_logo.png")
-        self.label.setScaledContents(True)
-        self.label.setPixmap(pixmap)
+        # self.label = QLabel()
+        # pixmap = QPixmap("static\pict_logo.png")
+        # self.label.setScaledContents(True)
+        # self.label.setPixmap(pixmap)
 
+        #   Current detection, Keywords and sentence label
+        self.current_detection_label = QLabel("<b>Currently Detecting</b>", parent=self)
+        self.current_detection_label.setStyleSheet(""" 
+        *{
+            height: fit-content
+        }
+        """)
+        self.detected_keywords_label = QLabel("<b>Detected keywords</b>", parent=self)
+        self.detected_keywords_label.setStyleSheet(""" 
+        *{
+            height: fit-content
+        }
+        """)
+        self.current_detection_placeholder = QLabel("Ready for detection", parent=self)
+        self.current_detection_placeholder.setStyleSheet("""
+        *{
+            background-color: rgb(3, 173, 252);
+            border-style: outset;
+            border-width: 0px;
+            border-radius: 10px;
+            color: white;
+            font: bold 14px;
+            padding: 2px;
+            height: fit-content;
+        }
+        """)
+        self.detected_keywords_placeholder = QLabel("Ready for detection", parent=self)
+        self.detected_keywords_placeholder.setStyleSheet("""
+                *{
+                    background-color: rgb(3, 173, 252);
+                    border-style: outset;
+                    border-width: 0px;
+                    border-radius: 10px;
+                    color: white;
+                    font: bold 14px;
+                    padding: 6px;
+                    height: fit-content;
+                }
+            """)
+        #   Buttons
         self.button = QPushButton("Connect to livefeed!")
         self.button.setStyleSheet("""
         *{
@@ -88,10 +129,15 @@ class MainWindow(QWidget):
         self.toggle_inference_button.setCheckable(True)
         self.toggle_inference_button.clicked.connect(self.toggle_inference)
         
-        layout.addWidget(self.label)
-        layout.addWidget(self.button)
-        layout.addWidget(self.confirm_button)
+        # layout.addWidget(self.label)
+        layout.addWidget(self.button)        
         layout.addWidget(self.toggle_inference_button)
+        layout.addWidget(self.current_detection_label)
+        layout.addWidget(self.current_detection_placeholder)
+        layout.addWidget(self.detected_keywords_label)
+        layout.addWidget(self.detected_keywords_placeholder)
+        layout.addWidget(self.confirm_button)
+        
         self.setLayout(layout)
 
     def toggle_feed(self):
@@ -110,6 +156,10 @@ class MainWindow(QWidget):
         keyword_list = self.webcamFeed.keywords
         self.webcamFeed.reset_occurence_counter()
         self.webcamFeed.keywords = []
+        #   TODO:   Call to API for sentence
+        #   Reset labels
+        self.update_detection_label("Reset for detection")
+        self.update_keywords_placeholder("Reset for detection")
         print(keyword_list)
 
     def toggle_inference(self):
@@ -119,6 +169,12 @@ class MainWindow(QWidget):
             self.toggle_inference_button.setText("Start signing")
         self.inference_allowed = not self.inference_allowed    
         self.webcamFeed.set_inference_status(allowed=self.inference_allowed)
+
+    def update_detection_label(self, detected_keyword):
+        self.current_detection_placeholder.setText("<b>" + detected_keyword + "</b>")
+
+    def update_keywords_placeholder(self, detected_keywords):
+        self.detected_keywords_placeholder.setText("<b>" + str(detected_keywords) + "</b>")
 
 #Create an instance of QApplication
 app = QApplication(sys.argv)
