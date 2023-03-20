2fd85f73440f8e681eace8053deda714ae3d65da
VaradMash <VaradMash@users.noreply.github.com>
2023-03-20 16:47:26 +0530

    Added constructor for model initialization.

--- sentence_generation.py
+++ sentence_generation.py
@@ -1,12 +1,15 @@
 from keytotext import pipeline
 
-
-
 class sentenceGeneration:
+
+    def __init__(self):
+        self.nlp = pipeline("k2t")
     
-    def generateSentence(self,keywords):
-        nlp = pipeline("k2t")
-        return print(nlp(keywords))
-keywords=['high','blood pressure', 'doctor','emergency']
-a=sentenceGeneration()
-a.generateSentence(keywords)
+    def generateSentence(self,keywords):        
+        print(self.nlp(keywords))
+        return self.nlp(keywords)
+    
+if __name__ == "__main__":    
+    keywords=['high','blood pressure', 'doctor','emergency']
+    a=sentenceGeneration()
+    a.generateSentence(keywords)
